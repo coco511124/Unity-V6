d5f37e636d2e0f4bc659ac4f7cd0696415499b54
qulick <58820014+Qulick-k@users.noreply.github.com>
2023-06-29 11:07:28 +0800

    微調canvas框
    在UILookAt微調canvas在旋轉時的穩定度

--- Assets/script/OculusScripts/OculusHead/UILookAt.cs
+++ Assets/script/OculusScripts/OculusHead/UILookAt.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 
 public class UILookAt : MonoBehaviour
 {
-    
+    public Transform head;    
+        
     private void LateUpdate()
     {
-        transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward, Camera.main.transform.rotation * Vector3.up);
+        //transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward, Camera.main.transform.rotation * Vector3.up);
+        transform.LookAt(new Vector3(head.position.x, transform.position.y, head.position.z)); //讓canvas實時面相鏡頭
+        transform.forward *= -1;      //上面那行程式會變反的，所以forward*-1讓canvas轉正
     }
 
   
