94483328802ceac69608410b2e879f4aa689508b
coco511124 <122720358+coco511124@users.noreply.github.com>
2023-08-05 17:08:23 +0800

    合成系統初步完成
    可偵測A物件來產生B物件

---
+++ Assets/script/OculusScripts/ScriptableObjectFolder/RecipeSO Folder/ItemSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemSO", menuName = "ItemSO")]
+
+public class ItemSO : ScriptableObject
+{
+    public GameObject prefab;
+}
