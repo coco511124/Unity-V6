552d74136ffb5472ad00062289d727c333beaf13
coco511124 <122720358+coco511124@users.noreply.github.com>
2023-08-26 20:30:49 +0800

    成功匯出成csv檔
    log紀錄可匯出成json及csv
    csv會亂碼，要手動設定才能正常顯示中文
    另外，目前是把WriteToCsv寫在SaveSystem裡面
    之後會改到FileHandler

--- Assets/script/OculusScripts/SaveSystem/Ch2/SaveSystemCh2.cs
+++ Assets/script/OculusScripts/SaveSystem/Ch2/SaveSystemCh2.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.IO;
 
 [System.Serializable]
 public class SaveSystemCh2 : MonoBehaviour
@@ -14,13 +15,18 @@ public class SaveSystemCh2 : MonoBehaviour
     [SerializeField] public string type;
     [SerializeField] PlayerDataCh2 playerDataCh2;
 
+    string FILEPATH;
+    string FILENAME;
+
     private void Start()
     {
         pldataCh2 = FileHandler.ReadFromJSON<PlayerDataCh2>(filename);
+        FILEPATH = Application.dataPath;
+        FILENAME = "/playerdatach2.csv";
     }
     public void Save()
     {
-        pldataCh2.Add(new PlayerDataCh2(playerName, DateTime.Now.ToString(), type));
+        pldataCh2.Add(new PlayerDataCh2(playerName, DateTime.Now.ToString(), type) { playerName = playerName, playerTime = DateTime.Now.ToString(), playerActionType = type });
         FileHandler.SaveToJSON<PlayerDataCh2>(pldataCh2, filename);
     }
     public void PickNorth()
@@ -58,6 +64,29 @@ public class SaveSystemCh2 : MonoBehaviour
         type = "¾ß¨ú¿}¤ô";
         Save();
     }
-    
-   
+    public void WriteToCsv(string FILENAME, List<PlayerDataCh2> pldata2)
+    {
+        using (var dataFile = new StreamWriter(FILEPATH + FILENAME))
+        {
+            dataFile.WriteLine(returnDataRowName());
+            foreach (var playerDataCh2 in pldata2)
+            {
+                dataFile.WriteLine($"{playerDataCh2.playerName}, {playerDataCh2.playerTime}, {playerDataCh2.playerActionType}");
+            }
+            dataFile.Close();
+        }
+    }
+    string returnDataRowName()
+    {
+        return "Name, Time, ActionType";
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            WriteToCsv(FILENAME, pldataCh2);
+            Debug.Log(FILEPATH + FILENAME);
+        }
+    }
+
 }
