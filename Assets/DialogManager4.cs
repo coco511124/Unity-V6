c8832bc2fcf076f71e5f525ba4065230a2bb3fde
coco511124 <122720358+coco511124@users.noreply.github.com>
2023-10-19 22:57:45 +0800

    匯入第四單元
    匯入第四單元
    加對話腳本

---
+++ Assets/DialogManager4.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogManager4 : MonoBehaviour
+{
+    public UnityEngine.UI.Text actorName;
+    public UnityEngine.UI.Text messageText;
+    public RectTransform backgroundBox;
+    public GameObject DB, XR;//, endPanel;
+    //public UnityEngine.UI.Text Mission1,Mission2, Mission3, Mission4, Mission5;未來再處理這邊
+
+    public GameObject portal;
+
+
+
+    public AudioSource typingSound;
+
+    Message4[] currentMessages;
+    Actor4[] currentActors;
+    int activeMessage = 0;
+
+
+
+    public void openDialogue(Message4[] messages, Actor4[] actors)
+    {
+        typingSound.Play();
+        currentActors = actors;
+        currentMessages = messages;
+        activeMessage = 0;
+        //Debug.Log(activeMessage);
+
+        Debug.Log("started conversation! loaded messages: " + messages.Length);
+        displayMessage();
+    }
+
+    void displayMessage()
+    {
+        Debug.Log("display message");
+        Message4 messageToDisplay = currentMessages[activeMessage];
+        messageText.text = messageToDisplay.meassage;
+        //Debug.Log(messageText.text);
+
+        Actor4 actorToDisplay = currentActors[messageToDisplay.actorId];
+        actorName.text = actorToDisplay.name;
+        //Debug.Log(actorToDisplay.name);
+        DB.SetActive(true);
+
+    }
+
+    public void NextMessage()
+    {
+        activeMessage++;
+        if (activeMessage < currentMessages.Length)
+        {
+            displayMessage();
+            typingSound.Play();
+
+        }
+
+        else
+        {
+            DB.SetActive(false);
+            if (XR.gameObject.tag == "Player")
+            {
+                portal.SetActive(true);
+            }
+        }
+    }
+
+    public void ClickNextMessage()
+    {
+        NextMessage();
+    }
+
+}
---
+++ Assets/script/OculusScripts/OculusDialog/Dialog_Ch4/DialogTrigger_Ch4.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.AI;
+
+public class DialogTrigger_Ch4 : MonoBehaviour
+{
+    public Message4[] messages_1, messages_2;
+    public Actor4[] actors;
+    public GameObject dialogueBox, dialogCanvas;
+    public AudioSource typingSound;
+
+
+
+    //[SerializeField] private GameObject ObjectTag;
+
+    private void Start()
+    {
+
+    }
+
+    void StartDialogue1()
+    {
+        dialogCanvas.GetComponent<DialogManager4>().openDialogue(messages_1, actors);
+    }
+
+    void StartDialogue2()
+    {
+        dialogCanvas.GetComponent<DialogManager4>().openDialogue(messages_2, actors);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {   
+        if(other !=null)
+        {
+            StartDialogue1();
+        }
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        //³]§PÂ_±ø¥ó
+        //©I¥sNPC_animate
+        dialogueBox.SetActive(false);
+        //ObjectTag.GetComponent<RandomPathTrolling>().SetWalkTrue();
+        //ObjectTag.GetComponent<NPC_animate>().BackAnimate();
+    }
+}
+
+[System.Serializable]
+public class Message4
+{
+    public int actorId;
+    public string meassage;
+}
+
+
+
+[System.Serializable]
+public class Actor4
+{
+    public string name;
+}
+   
