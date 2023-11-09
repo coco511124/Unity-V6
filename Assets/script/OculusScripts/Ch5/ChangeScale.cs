5e22f3ab031552dd01750093a12a3613d9da4e41
coco511124 <122720358+coco511124@users.noreply.github.com>
2023-11-09 18:18:41 +0800

    Ch5 socket完成
    Ch5 socket完成

---
+++ Assets/script/OculusScripts/Ch5/ChangeScale.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScale : MonoBehaviour
+{
+    private Vector3 Scale;
+    [SerializeField] private Transform NeedChangeObject;
+
+    private bool allowToBack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Scale = NeedChangeObject.localScale;
+        allowToBack = false;
+    }
+
+    public void ChangeObjectScale()
+    {
+        if (allowToBack == false)
+        {
+            NeedChangeObject.localScale = new Vector3(5f, 5f, 5f);
+            StartCoroutine(ChangeBoolToTrue());  //使用協程，等2秒再讓布林值變為true
+            Debug.Log("變換體積");
+        }
+        else
+        {
+            NeedChangeObject.localScale = Scale;
+            StartCoroutine(ChangeBoolToFalse()); // 使用協程，等2秒再讓布林值變為false
+            Debug.Log("還原大小");
+        }
+
+    }
+
+    IEnumerator ChangeBoolToTrue()
+    {
+        yield return new WaitForSeconds(2);
+        allowToBack = true;
+    }
+    IEnumerator ChangeBoolToFalse()
+    {
+
+        yield return new WaitForSeconds(2);
+        allowToBack = false;
+    }
+
+}
