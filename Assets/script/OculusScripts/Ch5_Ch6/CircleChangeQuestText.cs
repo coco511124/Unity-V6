bb1d23f3b008505d68ec2d3940127ed3388f7777
qulick <58820014+Qulick-k@users.noreply.github.com>
2023-12-16 20:41:22 +0800

    修正第六單元的單元總結內容
    [Update] 修正第六單元的單元總結內容
    [Update] 修正任務清單部分文字

---
+++ Assets/script/OculusScripts/Ch5_Ch6/CircleChangeQuestText.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CircleChangeQuestText : MonoBehaviour
+{
+    [SerializeField] private Text Mission1;
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.name == "XR Origin")
+        {
+            Mission1.text = "<color=green>✓ 1.前往紅色圈圈</color>";
+        }
+    }
+}
